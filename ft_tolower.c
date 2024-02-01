6e267d478c21cc0bfc7ac9854e9eb97441936d27
Raul Moreno Arroyo <ramoreno@cbr10s1.42barcelona.com>
2024-02-01 14:53:15 +0100

    added new functions

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 14:49:51 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/01/30 14:50:14 by ramoreno         ###   ########.fr       */
+/*   Created: 2024/02/01 14:32:19 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/02/01 14:35:27 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (c - ('a' - 'A'));
+	if (c >= 'A' && c <= 'Z')
+		return (c + ('a' - 'A'));
 	else
 		return (c);
 }
