36828d0b22611660133d128275b0540af7ac62f2
Raul Moreno Arroyo <ramoreno@cbr14s6.42barcelona.com>
2024-02-02 13:47:52 +0100

    strncmp added

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (*s1 && *s2 && *s1 == *s2 && i < (n-1))
+	while (*s1 && *s2 && *s1 == *s2 && i < (n - 1))
 	{
 		s1++;
 		s2++;
