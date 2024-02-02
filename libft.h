106d6ced0da35128b3ba973e74846f1f7090247b
Raul Moreno Arroyo <ramoreno@cbr14s6.42barcelona.com>
2024-02-02 13:01:10 +0100

    added strncmp

--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *restrict dst,
@@ -30,6 +31,7 @@ size_t		ft_strlcat(char *restrict dst,
 		const char *restrict src, size_t dstsize);
 
 char    *ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
 
 void			*ft_memset(void *s, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
