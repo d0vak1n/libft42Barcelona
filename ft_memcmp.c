9b84a19684cbad427c0b099b246a3302217231ee
Raul Moreno Arroyo <ramoreno@cbr14s3.42barcelona.com>
2024-02-07 15:59:46 +0100

    fixed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -13,20 +13,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
-	char	*str1;
-	char	*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	while (*str1 && *str2 && *str1 == *str2 && i < (n - 1))
-	{
-		str1++;
-		str2++;
-		i++;
-	}
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	return (*str1 - *str2);
+	while (i < (n - 1) && (str1[i] == str2[i]))
+		i++;
+	return (str1[i] - str2[i]);
 }
