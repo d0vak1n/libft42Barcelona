1df05f01a1853217f5398a1d88b9adcb82af2379
Raul <raul@raul.com>
2024-02-14 16:20:22 +0100

    fixed redundancy in function

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,37 +11,25 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-size_t	sizecontrol(size_t srcsize, size_t varsize, size_t dstsize)
-{
-	size_t	totalsize;
-
-	if (dstsize < varsize)
-		return (totalsize = srcsize + dstsize);
-	else
-		return (totalsize = srcsize + varsize);
-}
-
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
 	size_t	srcsize;
 	size_t	varsize;
-	size_t	totalsize;
 
 	j = 0;
 	srcsize = ft_strlen(src);
 	varsize = ft_strlen(dst);
-	totalsize = sizecontrol(srcsize, varsize, dstsize);
-	if (dstsize == 0)
-		return (totalsize);
+	if (dstsize <= varsize)
+		return (srcsize + dstsize);
 	i = varsize;
-	while (i < dstsize - 1 && dstsize - 1 > varsize)
+	while (src[j] && i < dstsize - 1)
 	{
 		dst[i] = src[j];
 		i++;
 		j++;
 	}
 	dst[i] = '\0';
-	return (totalsize);
+	return (srcsize + varsize);
 }
