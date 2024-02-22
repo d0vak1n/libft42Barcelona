8a21b0516c60f0678d6c6ebe4ac34e97a7e451a2
Raul <raul@raul.com>
2024-02-22 14:19:49 +0100

    aded putendl

--- libft.h
+++ libft.h
@@ -44,6 +44,7 @@ void	ft_bzero(void *s, size_t n);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
