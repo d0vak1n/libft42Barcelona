64d2a95d937d599a0ea1416a9b3b52e2e4887c5f
Raul <rulanxa@gmail.com>
2024-10-03 20:41:18 +0200

    struct declared

--- libft.h
+++ libft.h
@@ -15,7 +15,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
-
+typedef struct      s_list
+{
+    void			*content;
+    struct s_list	*next;
+}                   t_list;
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
